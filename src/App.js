3f107babe981e1199475b1c801143dc28a2099cf
hamaKazuya <haman.kazuya@gmail.com>
2020-03-04 11:17:08 +0900

    create func component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+
+
+// 関数コンポーネント
 function App() {
   // const greeting = 'hi!'
   // const dom = <h1 className="App">hello world!! {greeting}</h1>;
@@ -7,9 +10,14 @@ function App() {
     <React.Fragment>
       <label htmlFor="bar">bar</label>
       <input type="text" onChange={() => {console.log('true')}}></input>
+      <Cat /><Cat /><Cat /><Cat />
     </React.Fragment>
   )
   // return dom;
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
